4b6af8d4ff1e70c25fd6126d7cd66734e589beda
eoinbreen <e.breen185@gmail.com>
2023-07-11 21:44:06 +0100

    Learned more about selecting and changing elements

--- 2023_Version/16.The_Document_Object_Model/DOM Challenge/index.js
+++ 2023_Version/16.The_Document_Object_Model/DOM Challenge/index.js
@@ -1,6 +1,23 @@
-document.query
+
+// Style Documentation https://www.w3schools.com/jsref/dom_obj_style.asp
 
 function change_text(){
-    third = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild
-    third.innerHTML = "My Name is Eoin"
+    list_link = document.querySelector(".list a");
+    list_link.style.color = "red";
+    second = document.querySelectorAll("li")[1];
+    second.innerHTML = "Hello";
+    third = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+    third.innerHTML = "My Name is Eoin";
+}
+
+function checkbox(){
+    var checkbox = document.getElementById("myCheck");
+    var button = document.getElementsByClassName("myButton")[0];
+
+    if(checkbox.checked == true){
+        button.style.backgroundColor = "yellow";
+    }
+    else{
+        button.style.backgroundColor = "red";
+    }
 }
