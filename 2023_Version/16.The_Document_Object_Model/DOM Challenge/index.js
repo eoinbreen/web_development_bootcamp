53bd6ad5bb6a9e9633e8af2295866e76818fd62a
eoinbreen <e.breen185@gmail.com>
2023-07-12 21:06:32 +0100

    Learned how to Add, Remove and Toggle CSS classes using javascript

--- 2023_Version/16.The_Document_Object_Model/DOM Challenge/index.js
+++ 2023_Version/16.The_Document_Object_Model/DOM Challenge/index.js
@@ -2,12 +2,20 @@
 // Style Documentation https://www.w3schools.com/jsref/dom_obj_style.asp
 
 function change_text(){
+    first_link = document.querySelector("a")
+    first_link.textContent = "Yahoo"
+    first_link.setAttribute("href", "https://www.yahoo.com")
+    
     list_link = document.querySelector(".list a");
     list_link.style.color = "red";
+    
     second = document.querySelectorAll("li")[1];
     second.innerHTML = "Hello";
+    
     third = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
-    third.innerHTML = "My Name is Eoin";
+    third.textContent = "My Name is Eoin";
+    
+
 }
 
 function checkbox(){
@@ -19,5 +27,18 @@ function checkbox(){
     }
     else{
         button.style.backgroundColor = "red";
+    }
+}
+
+function visibility(){
+    var checkbox = document.querySelector("#visCheck");
+    var button = document.getElementsByClassName("myButton")[0];
+    document.querySelector("h1").classList.toggle("huge");
+
+    if(checkbox.checked == true){
+        button.classList.add("invisible");
+    }
+    else{
+        button.classList.remove("invisible");
     }
 }
