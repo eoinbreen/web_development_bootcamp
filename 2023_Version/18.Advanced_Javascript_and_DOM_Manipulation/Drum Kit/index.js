dcc09d031f2b27278fede5bf158e408b49edeb55
eoinbreen <e.breen185@gmail.com>
2023-07-17 17:22:50 +0100

    Used a Switch statement to play a different sound for each button

--- 2023_Version/18.Advanced_Javascript_and_DOM_Manipulation/Drum Kit/index.js
+++ 2023_Version/18.Advanced_Javascript_and_DOM_Manipulation/Drum Kit/index.js
@@ -4,10 +4,39 @@ buttons = document.querySelectorAll(".drum")
 for(var i = 0; i<buttons.length; i++)
 {
     buttons[i].addEventListener("click", function (){
-       button = this;
-       button.style.color = "white"
-       console.log(button.textContent + " Button was Pressed" )
+       button = this.innerHTML;
+       switch (button){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+        break
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+        break
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+        break
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+        break
+        case "j":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+        break
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+        break
+        case "l":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+        break
+        default:    // runs if no other case in statement runs
+            console.log(button)
+       }
     });
 }
-
 
