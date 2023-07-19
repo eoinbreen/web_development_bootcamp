4e8cbb22a1808d422df2302f169c00e84242ef28
eoinbreen <e.breen185@gmail.com>
2023-07-19 18:02:28 +0100

    Learned how to add JQuery to a website and how to select and change elements with it

---
+++ 2023_Version/19.JQuery/JQuery_Demo/index.js
@@ -0,0 +1,19 @@
+// JQuery Notes
+
+// Selecting Elements
+$("h1").css("color", "red");
+console.log($("h1").css("font-size")); // One input in css method gets a value, 2 inputs sets the value to the second input
+$("button").css("background-color", "orange");
+
+// Adding and removing Classes
+$("h1").addClass("big-title margin-100");
+$("h1").removeClass("big-title");
+console.log($("h1").hasClass("margin-100")); //Checks if element has a specific class
+
+// Text Manipulation
+$("h1").html("<em> Hello World </em>"); //innerHTML
+$("button").text("Don't Click Me"); //TextContent
+
+// Attribute Manipulation
+console.log($("a").attr("href"));
+$("a").attr("href","https://www.yahoo.com/" );
