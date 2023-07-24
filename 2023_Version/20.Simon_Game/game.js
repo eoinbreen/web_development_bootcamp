44a64d961503ec902e9c515a40b7aa013c619487
eoinbreen <e.breen185@gmail.com>
2023-07-24 17:13:05 +0100

    Made the next button in the sequence flash and play a sound

--- 2023_Version/20.Simon_Game/game.js
+++ 2023_Version/20.Simon_Game/game.js
@@ -1,9 +1,17 @@
 var button_colors = ["red", "blue", "green", "yellow"];
 var game_pattern = [];
 
+
 function nextSequence(){
     rand_num = Math.floor(Math.random() * 4);
     random_chosen_color = button_colors[rand_num];
     game_pattern.push(random_chosen_color);
-    console.log(game_pattern);
+
+    next_button = $("#"+random_chosen_color);
+    next_button.fadeOut(50).fadeIn(100);
+    var audio = new Audio("sounds/"+random_chosen_color+".mp3");
+    audio.play();
+    
+    //console.log(game_pattern);
 }
+
