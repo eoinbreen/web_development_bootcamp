4e7e81d87b89c28e359f74c3b8127f51f63b8112
eoinbreen <e.breen185@gmail.com>
2023-08-11 17:19:41 +0100

    Added form to create tasks and used body-parser to read form

--- 2023_Version/26.To-Do-List_Application/index.js
+++ 2023_Version/26.To-Do-List_Application/index.js
@@ -1,21 +1,54 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//Add a Task Page
+
+var tasks = [];
+
+function createTask(req){
+  var task_type = req.body["task-type"];
+  var task_description = req.body["task-description"];
+  var task_priority = req.body["task-priority"];
+  var task_date = req.body["task-date"];
+
+  var task = {
+    type: task_type,
+    description: task_description,
+    priority: task_priority,
+    date: task_date
+  }
+
+  tasks.push(task);
+}
+
+
+
 
 app.get("/", (req, res) => {
-  res.render("index.ejs")
+  res.render("index.ejs");
 });
 
-app.get("/work", (req, res) => {
-  res.render("work.ejs")
+app.post("/submit", (req, res) => {
+  createTask(req);
+  console.log(tasks);
 });
 
 app.get("/daily", (req, res) => {
-  res.render("daily.ejs")
+  res.render("daily.ejs");
 });
+
+app.get("/work", (req, res) => {
+  res.render("work.ejs");
+});
+
+
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
